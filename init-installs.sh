39f094d53ee66c2e4cb435282203b03c70fe32eb
Rossh87 <hunter.dev.87@gmail.com>
2022-08-02 13:57:21 -0500

    add tmux install

--- init-installs.sh
+++ init-installs.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-exec ./installs/github-cli.sh 2>./logs/github-cli-err.txt
+exec ./installs/github-cli.sh 2>./logs/github-cli-err.txt && \
+exec ./installs/tmux.sh 2>./logs/tmux-err.txt
